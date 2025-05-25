717e3e2f83746a4f97c4e248837c6f537fe3beec
Talha Balaj <talhabalaaj@gmail.com>
2025-05-25 11:52:47 +0500

    Bye bye

--- README.md
+++
@@ -1,2 +0,0 @@
-# Personal Website
-Hey there, this is source code my website [talhabalaj.com](https://talhabalaj.com). It's built with React using Static Site Generation with NextJS and styled-components for styling. 
--- components/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemeProvider } from "styled-components";
-import { theme, GlobalStyles } from "../config/theme";
-
-export const Layout = ({ children }) => (
-    <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        {children}
-    </ThemeProvider>
-)
